cbccb2f58c9cca6d5fa7c5d16b6b86780070382c
Ruth Reyes Castellin <rreyes@bizware.cl>
2021-02-18 22:39:53 -0500

    avances

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -29,8 +29,16 @@ export const navItems: INavData[] = [
     url: "/productos/planes",
     icon: "icon-speedometer",
   },
- 
+  {
+    title: true,
+    name: "Estadisticas",
+  },
 
+  {
+    name: "Visitas",
+    url: "/base",
+    icon: "cil-graph",
+  },
   {
     title: true,
     name: "Usuarios",
@@ -40,175 +48,6 @@ export const navItems: INavData[] = [
     url: "/usuarios",
     icon: "icon-handbag",
   },
-  
-
-  {
-    title: true,
-    name: "Estadisticas",
-  },
-
-
-
-  {
-    name: "Visitas",
-    url: "/base",
-    icon: "cil-graph",
-    // children: [
-    //   {
-    //     name: "Cards",
-    //     url: "/base/cards",
-    //     icon: "icon-puzzle",
-    //   },
-    //   {
-    //     name: "Carousels",
-    //     url: "/base/carousels",
-    //     icon: "icon-puzzle",
-    //   },
-    //   {
-    //     name: "Collapses",
-    //     url: "/base/collapses",
-    //     icon: "icon-puzzle",
-    //   },
-    //   {
-    //     name: "Forms",
-    //     url: "/base/forms",
-    //     icon: "icon-puzzle",
-    //   },
-    //   {
-    //     name: "Navbars",
-    //     url: "/base/navbars",
-    //     icon: "icon-puzzle",
-    //   },
-    //   {
-    //     name: "Pagination",
-    //     url: "/base/paginations",
-    //     icon: "icon-puzzle",
-    //   },
-    //   {
-    //     name: "Popovers",
-    //     url: "/base/popovers",
-    //     icon: "icon-puzzle",
-    //   },
-    //   {
-    //     name: "Progress",
-    //     url: "/base/progress",
-    //     icon: "icon-puzzle",
-    //   },
-    //   {
-    //     name: "Switches",
-    //     url: "/base/switches",
-    //     icon: "icon-puzzle",
-    //   },
-    //   {
-    //     name: "Tables",
-    //     url: "/base/tables",
-    //     icon: "icon-puzzle",
-    //   },
-    //   {
-    //     name: "Tabs",
-    //     url: "/base/tabs",
-    //     icon: "icon-puzzle",
-    //   },
-    //   {
-    //     name: "Tooltips",
-    //     url: "/base/tooltips",
-    //     icon: "icon-puzzle",
-    //   },
-    // ],
-  },
-  // {
-  //   name: "Buttons",
-  //   url: "/buttons",
-  //   icon: "icon-cursor",
-  //   children: [
-  //     {
-  //       name: "Buttons",
-  //       url: "/buttons/buttons",
-  //       icon: "icon-cursor",
-  //     },
-  //     {
-  //       name: "Dropdowns",
-  //       url: "/buttons/dropdowns",
-  //       icon: "icon-cursor",
-  //     },
-  //     {
-  //       name: "Brand Buttons",
-  //       url: "/buttons/brand-buttons",
-  //       icon: "icon-cursor",
-  //     },
-  //   ],
-  // },
-  // {
-  //   name: "Charts",
-  //   url: "/charts",
-  //   icon: "icon-pie-chart",
-  // },
-  // {
-  //   name: "Icons",
-  //   url: "/icons",
-  //   icon: "icon-star",
-  //   children: [
-  //     {
-  //       name: "CoreUI Icons",
-  //       url: "/icons/coreui-icons",
-  //       icon: "icon-star",
-  //       badge: {
-  //         variant: "success",
-  //         text: "NEW",
-  //       },
-  //     },
-  //     {
-  //       name: "Flags",
-  //       url: "/icons/flags",
-  //       icon: "icon-star",
-  //     },
-  //     {
-  //       name: "Font Awesome",
-  //       url: "/icons/font-awesome",
-  //       icon: "icon-star",
-  //       badge: {
-  //         variant: "secondary",
-  //         text: "4.7",
-  //       },
-  //     },
-  //     {
-  //       name: "Simple Line Icons",
-  //       url: "/icons/simple-line-icons",
-  //       icon: "icon-star",
-  //     },
-  //   ],
-  // },
-  // {
-  //   name: "Notifications",
-  //   url: "/notifications",
-  //   icon: "icon-bell",
-  //   children: [
-  //     {
-  //       name: "Alerts",
-  //       url: "/notifications/alerts",
-  //       icon: "icon-bell",
-  //     },
-  //     {
-  //       name: "Badges",
-  //       url: "/notifications/badges",
-  //       icon: "icon-bell",
-  //     },
-  //     {
-  //       name: "Modals",
-  //       url: "/notifications/modals",
-  //       icon: "icon-bell",
-  //     },
-  //   ],
-  // },
-  // {
-  //   name: "Widgets",
-  //   url: "/widgets",
-  //   icon: "icon-calculator",
-  //   badge: {
-  //     variant: "info",
-  //     text: "NEW",
-  //   },
-  // },
   {
     divider: true,
   },
@@ -231,26 +70,6 @@ export const navItems: INavData[] = [
         url: "/theme/typography",
         icon: "cil-stream",
       },
-      // {
-      //   name: "Login",
-      //   url: "/login",
-      //   icon: "icon-star",
-      // },
-      // {
-      //   name: "Register",
-      //   url: "/register",
-      //   icon: "icon-star",
-      // },
-      // {
-      //   name: "Error 404",
-      //   url: "/404",
-      //   icon: "icon-star",
-      // },
-      // {
-      //   name: "Error 500",
-      //   url: "/500",
-      //   icon: "icon-star",
-      // },
     ],
   },
   // {
