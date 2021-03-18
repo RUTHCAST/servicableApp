6ec12a93b9d0f2521c016cb3c8222a811260d6c3
Ruth Reyes Castellin <rreyes@bizware.cl>
2021-03-18 12:51:18 -0500

    avences y mejoras

--- src/app/modules/products/components/categories/lists/list.component.ts
+++ src/app/modules/products/components/categories/lists/list.component.ts
@@ -48,10 +48,10 @@ export class ListComponent implements OnInit, OnDestroy {
         zeroRecords: "No hay data para mostrar",
         emptyTable: "Sin registros para mostrar",
         paginate: {
-          first: "<<",
+          first: "Primero",
           previous: "Anterior",
           next: "Siguiente",
-          last: ">>",
+          last: "Ultimo",
         },
       },
     };
